4c35f9b9ed15aa24a88632a2713581340b1963a9
devebe <dvbirdja@gmail.com>
2022-08-26 20:35:12 +0200

    Add divide function to js

--- JS/calculator.js
+++ JS/calculator.js
@@ -19,7 +19,15 @@ function multiply(...args) {
     for (let i = 1; i < args.length; i++) {
         result *= args[i];
     }
-    return result
+    return result;
+}
+
+function divide(...args) {
+    let result = args[0];
+    for (let i = 1; i < args.length; i++) {
+        result /= args[i];
+    }
+    return result;
 }
 
 console.log(add(5,10,15,16,22));
@@ -27,3 +35,5 @@ console.log(add(5,10,15,16,22));
 console.log(subtract(100,10,15,20,25));
 
 console.log(multiply(3,3,2,2,1));
+
+console.log(divide(40,2,2));
